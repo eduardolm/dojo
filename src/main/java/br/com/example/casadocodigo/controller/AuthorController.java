d1073913ad5451946106b3cee4a98da5ea4b216a
Eduardo Lodi Marzano <lodi001@uol.com.br>
2021-05-02 20:29:03 -0300

    Created controller

--- src/main/java/br/com/example/casadocodigo/controller/AuthorController.java
+++ src/main/java/br/com/example/casadocodigo/controller/AuthorController.java
@@ -1,5 +1,25 @@
 package br.com.example.casadocodigo.controller;
 
+import br.com.example.casadocodigo.dto.AuthorDto;
+import br.com.example.casadocodigo.entity.Author;
+import br.com.example.casadocodigo.repository.AuthorRepository;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
 @RestController
 public class AuthorController {
+
+    private final AuthorRepository repository;
+
+    public AuthorController(AuthorRepository repository) {
+        this.repository = repository;
+    }
+
+    @PostMapping
+    public Author create(@RequestBody @Valid AuthorDto authorDto) {
+
+    }
 }
